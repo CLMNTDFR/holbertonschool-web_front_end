8928bc10db2283e452979d01f0b86d1bfe8fc99f
CLMNTDFR <deferclement59@gmail.com>
2026-08-14 16:52:33 +0200

    Remove all JavaScript files from the Javascript_advanced directory

--- Javascript_advanced/0-welcome.js
+++
@@ -1,8 +0,0 @@
-function welcome(firstName, lastName) {
-    const fullName = firstName + " " + lastName;
-
-    function displayFullName() {
-        alert(`Welcome ${fullName}!`);
-    }
-    displayFullName();
-}
--- Javascript_advanced/2-function_me.js
+++
@@ -1,7 +0,0 @@
-function welcomeMessage(fullName) {
-    alert(`Welcome ${fullName}`);
-}
-
-const guillaume = welcomeMessage("Guillaume");
-const alex = welcomeMessage("Alex");
-const fred = welcomeMessage("Fred");
